b43104dd568da7660c94dba8dd6d7ef8f315292b
Rajkumar S <rajkumaar2304@icloud.com>
2020-04-05 10:39:26 +0530

    Initial commit :fire:
    Signed-off-by: Rajkumar S <rajkumaar2304@icloud.com>

---
+++ README.md
@@ -0,0 +1,19 @@
+# Unofficial PlayStore API
+
+> A helper tool that can be used in combination with shields.io for generating android app related badges
+
+## Usage
+
+Available Attributes : 
+```json
+{
+  "package": "in.co.rajkumaar.amritarepo",
+  "appVersion": "4.2.2",
+  "appSize": "4.5M",
+  "noOfInstalls": "5,000+",
+  "lastUpdated": "March 31, 2020",
+  "rating": "4.7",
+  "noOfUsersRated": "767",
+  "developer": "Rajkumar S"
+}
+```
---
+++ api.php
@@ -0,0 +1,95 @@
+<?php
+
+use Symfony\Component\DomCrawler\Crawler;
+
+/**
+ * Copyright (c) 2020 | RAJKUMAR S (http://rajkumaar.co.in)
+ */
+class API
+{
+    /**
+     * @var string
+     */
+    private $playstoreURL;
+    /**
+     * @var Crawler
+     */
+    private $crawler;
+    private $packageID;
+
+    public function __construct($package)
+    {
+        $this->packageID = $package;
+        $this->playstoreURL = 'https://play.google.com/store/apps/details?id=' . $this->packageID;
+        if ($html = file_get_contents($this->playstoreURL)) {
+            $this->crawler = new Crawler($html);
+        } else {
+            throw new Exception("Invalid Package ID");
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPackageID()
+    {
+        return $this->packageID;
+    }
+
+    /**
+     * @return string
+     */
+    public function getVersion()
+    {
+        return $this->crawler->filter('.htlgb')->eq(6)->text();
+    }
+
+    /**
+     * @return string
+     */
+    public function getInstalls()
+    {
+        return $this->crawler->filter('.htlgb')->eq(5)->text();
+    }
+
+    /**
+     * @return string
+     */
+    public function getSize()
+    {
+        return $this->crawler->filter('.htlgb')->eq(3)->text();
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastUpdated()
+    {
+//        die(var_dump($this->crawler->filter('.EymY4b')));
+        return $this->crawler->filter('.htlgb')->eq(1)->text();
+    }
+
+    /**
+     * @return string
+     */
+    public function getRating()
+    {
+        return $this->crawler->filter('.BHMmbe')->eq(0)->text();
+    }
+
+    /**
+     * @return string
+     */
+    public function getNoOfUsersRated()
+    {
+        return filter_var($this->crawler->filter('.EymY4b')->eq(0)->text(), FILTER_SANITIZE_NUMBER_INT);
+    }
+
+    /**
+     * @return string
+     */
+    public function getDeveloper()
+    {
+        return $this->crawler->filter('.htlgb')->eq(18)->text();
+    }
+}
