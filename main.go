e0bdf58f1f3b3318e68686e71c459241847847db
Rajkumar <rajkumaar2304@icloud.com>
2023-12-07 15:04:37 -0700

    add godotenv integration

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+
 	router := gin.Default()
 	router.GET("/", getREADME)
 	router.GET("/json", getAllData)
 	router.GET("/:key", getDataByKey)
-	router.Run("localhost:5003")
+	router.Run(fmt.Sprintf("localhost:%s", os.Getenv("SERVER_PORT")))
 }
